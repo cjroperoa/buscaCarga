9f493e4e28a365c236a5325ea31f4c3bc32f7c6e
cjropero <cjavieramaya@gmail.com>
2019-06-11 21:28:04 -0500

    (feat) => created methods of authentication & init checkToken constructor  @cjropero

--- App/buscaCarga/src/app/Services/authentication.service.ts
+++ App/buscaCarga/src/app/Services/authentication.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Platform } from '@ionic/angular';
+// import storage and platform
+import { Storage } from "@ionic/storage";
+
+
+// create const with TOKEN
+const TOKEN_KEY = 'auth-token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  // Object instance sesion
+  authenticationState = new BehaviorSubject(false);
+
+  constructor(
+    // instance constructor
+    private storage: Storage,
+    private platform: Platform
+  ) {
+  this.platform.ready().then(() => {
+    this.checkToken();
+  })
+   }
+
+  login() {
+    // 
+    return  this.storage.set(TOKEN_KEY, 'Bearer 123456').then(res => {
+      this.authenticationState.next(true);
+    });
+  }
+
+  logout() {
+    return this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+  
+  isActivated() {
+    this.authenticationState.value;
+  }
+
+  checkToken() {
+    return this.storage.get(TOKEN_KEY).then(res => {
+      if (res) {
+        this.authenticationState.next(true);
+      }
+    });
+  }
+
 }
