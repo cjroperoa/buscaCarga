8fcd5f70d9b2d5977153691dc2dbf8c164721611
cjropero <cjavieramaya@gmail.com>
2019-06-15 10:55:41 -0500

    (feat) => Relocating routing app routes @cjropero

--- App/buscaCarga/src/app/app-routing.module.ts
+++ App/buscaCarga/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './Services/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -9,29 +10,16 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: './Pages/login/login.module#LoginPageModule'
+    loadChildren: './Public/login/login.module#LoginPageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
+  { path: 'registry', 
+    loadChildren: './Public/registry/registry.module#RegistryPageModule' 
   },
-  { path: 'login', loadChildren: './Pages/login/login.module#LoginPageModule' },
-  { path: 'registry', loadChildren: './Pages/registry/registry.module#RegistryPageModule' },
-  { path: 'new-shipment-form', loadChildren: './Pages/new-shipment-form/new-shipment-form.module#NewShipmentFormPageModule' },
-  { path: 'shipment-details', loadChildren: './Pages/shipment-details/shipment-details.module#ShipmentDetailsPageModule' },
-  { path: 'new-shipment-category', loadChildren: './Pages/new-shipment-category/new-shipment-category.module#NewShipmentCategoryPageModule' },  { path: 'search', loadChildren: './Pages/search/search.module#SearchPageModule' },
-  { path: 'shipment-details-transporter', loadChildren: './Pages/shipment-details-transporter/shipment-details-transporter.module#ShipmentDetailsTransporterPageModule' },
-  { path: 'offer', loadChildren: './Pages/offer/offer.module#OfferPageModule' },
-  { path: 'my-offers', loadChildren: './Pages/my-offers/my-offers.module#MyOffersPageModule' },
-  { path: 'see-offer', loadChildren: './Pages/see-offer/see-offer.module#SeeOfferPageModule' },
-  { path: 'add-car-transporter', loadChildren: './Pages/add-car-transporter/add-car-transporter.module#AddCarTransporterPageModule' },
-  { path: 'membership-photo', loadChildren: './Pages/membership-photo/membership-photo.module#MembershipPhotoPageModule' },
-  { path: 'offer-approval-conveyor', loadChildren: './Pages/offer-approval-conveyor/offer-approval-conveyor.module#OfferApprovalConveyorPageModule' },
-  { path: 'offer-approval-sender', loadChildren: './Pages/offer-approval-sender/offer-approval-sender.module#OfferApprovalSenderPageModule' },
-  { path: 'my-publishings', loadChildren: './Pages/my-publishings/my-publishings.module#MyPublishingsPageModule' }
-
-
-
+  // {
+  //   path: 'pages',
+  //   canActivate: [AuthGuardService],
+  //   loadChildren: './Pages/pages-routing#MemberRoutingModule'
+  // }
 ];
 
 @NgModule({
