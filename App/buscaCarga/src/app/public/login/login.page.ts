f4dd702cb4769dbf4afc30e779d924a1af801c89
cjropero <cjavieramaya@gmail.com>
2019-06-15 15:32:49 -0500

    (feat) => add event button 'INGRESAR' login() @cjropero

--- App/buscaCarga/src/app/public/login/login.page.ts
+++ App/buscaCarga/src/app/public/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+  
+  }
+
+  login() {
+    this.authService.login();
   }
 
 }
