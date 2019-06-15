a42913c137252f07cf932c6723e47882e80c642f
cjropero <cjavieramaya@gmail.com>
2019-06-15 10:50:48 -0500

    (feat) => creating authenGuard service methods and canActivated injection @cjropero

--- App/buscaCarga/src/app/Services/auth-guard.service.ts
+++ App/buscaCarga/src/app/Services/auth-guard.service.ts
@@ -1,9 +1,16 @@
+import { AuthenticationService } from './authentication.service';
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
+
+  canActivate(): boolean {
+    return this.authService.isActivated();
+  }
+
 }
